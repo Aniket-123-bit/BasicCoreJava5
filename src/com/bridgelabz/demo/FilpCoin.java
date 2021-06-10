06f2ad88fa3c15a5b246b3d6ceb3939b37bf17e9
Aniket-123-bit <anisunny782@gmail.com>
2021-06-10 14:25:18 +0530

    Commit Flip Coin Push

---
+++ src/com/bridgelabz/demo/FilpCoin.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.demo;
+public class FilpCoin {
+	public static void main(String[] args) {
+		int loopCount = 1;
+		double result;
+		while(loopCount < 2)
+		{
+
+			result = Math.random();
+
+			if(result <= .5)
+
+				System.out.println(" Heads.");
+
+			else
+
+				System.out.println(" Tails.");
+
+			loopCount = loopCount + 1;
+
+		}
+
+	}
+
+}
+
+ 
